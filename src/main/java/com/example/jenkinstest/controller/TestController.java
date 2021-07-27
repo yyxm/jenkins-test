cde087ba8997b116d5608a7811a125cb36177b27
yyxm <yuxinming0012@163.com>
2021-07-27 11:58:34 +0800

    first commit

---
+++ src/main/java/com/example/jenkinstest/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yyxm
+ * @date 2021/7/27 11:55 上午
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
